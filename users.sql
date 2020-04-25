055575a60b194d6cc9f22b4bf2544c0c62f8c3f6
juhoPark <wngh7247@gmail.com>
2020-04-25 22:43:47 +0900

    Add Initial administrator registration

--- users.sql
+++ users.sql
@@ -1,3 +1,4 @@
+/* Create users table */
 CREATE TABLE `users` (
 	`id` INT(11) NOT NULL AUTO_INCREMENT,
 	`name` VARCHAR(20) NOT NULL COLLATE 'utf8_general_ci',
@@ -12,3 +13,11 @@ COLLATE='utf8_general_ci'
 ENGINE=InnoDB
 AUTO_INCREMENT=1
 ;
+
+/* 
+ * Initial administrator registration 
+ * email: admin1111@admin.com
+ * password: Admin1234!@
+*/
+INSERT INTO `users` (`name`, `nickname`, `password`, `phone`, `email`, `gender`) 
+VALUES ('관리자', 'admin', '$2y$10$M1dEXNN5Se8MJTa5n5pGCOuuGMAdQjPCMeMXpEIBAB2zvnQFnlM0e', '01012345678', 'admin1111@admin.com', 'M');
